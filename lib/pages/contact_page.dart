b1f0d453e629c493dafa5f6d599c63936ec4e3b8
Bruno Souza <bruno.arubesu@gmail.com>
2021-04-08 21:44:09 -0300

    feat: alert user when contact change to avoid lost data

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -19,7 +19,7 @@ class _ContactPageState extends State<ContactPage> {
   var _emailController = TextEditingController();
   var _phoneController = TextEditingController();
   var _nameFocus = FocusNode();
-  bool _userEdited;
+  bool _isContactEdited;
 
   @override
   void initState() {
@@ -29,7 +29,7 @@ class _ContactPageState extends State<ContactPage> {
       _editedContact = Contact();
       return;
     }
-
+    _isContactEdited = false;
     _editedContact = Contact.fromMap(widget.contact.toMap());
     _nameController.text = _editedContact.name;
     _emailController.text = _editedContact.email;
@@ -38,64 +38,97 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_editedContact.name ?? 'New Contact'),
-        backgroundColor: Colors.red,
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
-              Navigator.pop(context, _editedContact);
-            } else {
-              FocusScope.of(context).requestFocus(_nameFocus);
-            }
-          },
-          child: Icon(Icons.save),
-          backgroundColor: Colors.red),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: <Widget>[
-            Container(
-                width: 140,
-                height: 140,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: _editedContact.img != null
-                            ? FileImage(File(_editedContact.img))
-                            : AssetImage("images/profile.png")))),
-            TextField(
-                focusNode: _nameFocus,
-                decoration: InputDecoration(labelText: "Name"),
-                controller: _nameController,
-                keyboardType: TextInputType.text,
-                onChanged: (text) {
-                  _userEdited = true;
-                  setState(() {
-                    _editedContact.name = text;
-                  });
-                }),
-            TextField(
-                decoration: InputDecoration(labelText: "Email"),
-                controller: _emailController,
-                keyboardType: TextInputType.text,
-                onChanged: (text) {
-                  _userEdited = true;
-                  _editedContact.email = text;
-                }),
-            TextField(
-                decoration: InputDecoration(labelText: "Phone"),
-                controller: _phoneController,
-                keyboardType: TextInputType.text,
-                onChanged: (text) {
-                  _editedContact.phone = text;
-                }),
-          ],
+    return WillPopScope(
+      onWillPop: _alertContactEdited,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(_editedContact.name ?? 'New Contact'),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              if (_editedContact.name != null &&
+                  _editedContact.name.isNotEmpty) {
+                Navigator.pop(context, _editedContact);
+              } else {
+                FocusScope.of(context).requestFocus(_nameFocus);
+              }
+            },
+            child: Icon(Icons.save),
+            backgroundColor: Colors.red),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              Container(
+                  width: 140,
+                  height: 140,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: _editedContact.img != null
+                              ? FileImage(File(_editedContact.img))
+                              : AssetImage("images/profile.png")))),
+              TextField(
+                  focusNode: _nameFocus,
+                  decoration: InputDecoration(labelText: "Name"),
+                  controller: _nameController,
+                  keyboardType: TextInputType.text,
+                  onChanged: (text) {
+                    _isContactEdited = true;
+                    setState(() {
+                      _editedContact.name = text;
+                    });
+                  }),
+              TextField(
+                  decoration: InputDecoration(labelText: "Email"),
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (text) {
+                    _isContactEdited = true;
+                    _editedContact.email = text;
+                  }),
+              TextField(
+                  decoration: InputDecoration(labelText: "Phone"),
+                  controller: _phoneController,
+                  keyboardType: TextInputType.phone,
+                  onChanged: (text) {
+                    _editedContact.phone = text;
+                  }),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<bool> _alertContactEdited() {
+    if (_isContactEdited) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text("Your changes have not been saved"),
+              actions: [
+                TextButton(
+                    child: Text("Discard"),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                    }),
+                TextButton(
+                    child: Text("Save"),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context, _editedContact);
+                    }),
+              ],
+            );
+          });
+
+      return Future.value(false);
+    }
+    return Future.value(true);
   }
 }
